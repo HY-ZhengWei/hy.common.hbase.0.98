d8be14efa3a8e064c564321c65d3716d7c8ee9bf
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-06-06 22:20:06 +0800

    1.5.0
    Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

--- src/org/hy/common/hbase/HBase.java
+++ src/org/hy/common/hbase/HBase.java
@@ -421,7 +421,7 @@ public class HBase implements Cloneable
         Collection<HColumnDescriptor> v_HColumns = this.getTableFamilys(i_TableName);
         List<String>                  v_Ret      = new ArrayList<String>();
         
-        if ( JavaHelp.isNull(v_HColumns) )
+        if ( !JavaHelp.isNull(v_HColumns) )
         {
             for (HColumnDescriptor v_HColumn : v_HColumns)
             {
